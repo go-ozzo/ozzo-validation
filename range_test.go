e8b5b4fdcb7d418a05b208ce3bb96862c076c658
sgleizes <stephane.gleizes@gmail.com>
2016-09-01 00:58:20 +0200

    Use the right copyright header...

--- range_test.go
+++ range_test.go
@@ -1,8 +1,6 @@
-// Copyright (C) 2016 Etix Labs - All Rights Reserved.
-// All information contained herein is, and remains the property of Etix Labs and its suppliers,
-// if any. The intellectual and technical concepts contained herein are proprietary to Etix Labs
-// Dissemination of this information or reproduction of this material is strictly forbidden unless
-// prior written permission is obtained from Etix Labs.
+// Copyright 2016 Qiang Xue. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
 
 package validation
 
