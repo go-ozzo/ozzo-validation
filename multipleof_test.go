171021dad8e83b86a474fd330f8cb26d01a7ac49
Radilov Andrey <radilov.andrey@gmail.com>
2018-07-17 12:01:36 +0300

    new rule: multiple of

--- multipleof_test.go
+++ multipleof_test.go
@@ -14,13 +14,16 @@ func TestMultipleof(t *testing.T) {
 	r := MultipleOf(int(10))
 	assert.Equal(t, "must be multiple of 10", r.Validate(int(11)).Error())
 	assert.Equal(t, nil, r.Validate(int(20)))
+	assert.Equal(t, "cannot convert float32 to int64", r.Validate(float32(20)).Error())
+
+	r2 := MultipleOf("some string ....")
+	assert.Equal(t, "type not supported: string", r2.Validate(10).Error())
 
 	r3 := MultipleOf(uint(10))
 	assert.Equal(t, "must be multiple of 10", r3.Validate(uint(11)).Error())
 	assert.Equal(t, nil, r3.Validate(uint(20)))
+	assert.Equal(t, "cannot convert float32 to uint64", r3.Validate(float32(20)).Error())
 
-	r2 := MultipleOf("some string ....")
-	assert.Equal(t, "type not supported: string", r2.Validate(10).Error())
 }
 
 func Test_Multipleof_Error(t *testing.T) {
