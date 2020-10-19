c0d3bb3ac5f43ebb9fc22eaeb7d8c07836b7973b
Nathan Baulch <nathan.baulch@gmail.com>
2020-10-19 22:38:11 +1100

    Validate untyped maps similar to structs (#128)
    * Validate untyped maps similar to structs
    
    * Map validation refinements based on feedback
    
    * Flag map keys as optional

--- example_test.go
+++ example_test.go
@@ -154,3 +154,39 @@ func Example_six() {
 	// unexpected value
 	// <nil>
 }
+
+func Example_seven() {
+	c := map[string]interface{}{
+		"Name":  "Qiang Xue",
+		"Email": "q",
+		"Address": map[string]interface{}{
+			"Street": "123",
+			"City":   "Unknown",
+			"State":  "Virginia",
+			"Zip":    "12345",
+		},
+	}
+
+	err := validation.Validate(c,
+		validation.Map(
+			// Name cannot be empty, and the length must be between 5 and 20.
+			validation.Key("Name", validation.Required, validation.Length(5, 20)),
+			// Email cannot be empty and should be in a valid email format.
+			validation.Key("Email", validation.Required, is.Email),
+			// Validate Address using its own validation rules
+			validation.Key("Address", validation.Map(
+				// Street cannot be empty, and the length must between 5 and 50
+				validation.Key("Street", validation.Required, validation.Length(5, 50)),
+				// City cannot be empty, and the length must between 5 and 50
+				validation.Key("City", validation.Required, validation.Length(5, 50)),
+				// State cannot be empty, and must be a string consisting of two letters in upper case
+				validation.Key("State", validation.Required, validation.Match(regexp.MustCompile("^[A-Z]{2}$"))),
+				// State cannot be empty, and must be a string consisting of five digits
+				validation.Key("Zip", validation.Required, validation.Match(regexp.MustCompile("^[0-9]{5}$"))),
+			)),
+		),
+	)
+	fmt.Println(err)
+	// Output:
+	// Address: (State: must be in a valid format; Street: the length must be between 5 and 50.); Email: must be a valid email address.
+}
