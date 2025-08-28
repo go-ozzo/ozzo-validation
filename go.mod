43cdda80d8456f0f6f09980c47740f9c4400a58c
daetabased <153883715+daetabased@users.noreply.github.com>
2025-08-28 13:04:22 +0900

    add ULID validator

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/daetabased/ozzo
 go 1.13
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/stretchr/testify v1.4.0
 )
--- is/rules.go
+++ is/rules.go
@@ -59,6 +59,8 @@ var (
 	ErrUUIDv5 = ozzo.NewError("validation_is_uuid_v5", "must be a valid UUID v5")
 	// ErrUUID is the error that returns in case of an invalid UUID value.
 	ErrUUID = ozzo.NewError("validation_is_uuid", "must be a valid UUID")
+	// ErrULID is the error that returns in case of an invalid ULID value.
+	ErrULID = ozzo.NewError("validation_is_ulid", "must be a valid ULID")
 	// ErrCreditCard is the error that returns in case of an invalid credit card number.
 	ErrCreditCard = ozzo.NewError("validation_is_credit_card", "must be a valid credit card number")
 	// ErrISBN10 is the error that returns in case of an invalid ISBN-10 value.
@@ -172,6 +174,8 @@ var (
 	UUIDv5 = ozzo.NewStringRuleWithError(govalidator.IsUUIDv5, ErrUUIDv5)
 	// UUID validates if a string is a valid UUID
 	UUID = ozzo.NewStringRuleWithError(govalidator.IsUUID, ErrUUID)
+	// ULID validates if a string is a valid ULID
+	ULID = ozzo.NewStringRuleWithError(govalidator.IsULID, ErrULID)
 	// CreditCard validates if a string is a valid credit card number
 	CreditCard = ozzo.NewStringRuleWithError(govalidator.IsCreditCard, ErrCreditCard)
 	// ISBN10 validates if a string is an ISBN version 10
